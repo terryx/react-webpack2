4ad38685156202cb99b8e6720e76ec3ff3b548ef
TerryX <terryxlife@gmail.com>
2016-12-08 13:11:35 +0800

    Update package, minor clean up

--- src/app.js
+++ src/app.js
@@ -1,18 +1,14 @@
-// const React, { Component } from 'react'
-// const { render } from 'react-dom'
-//
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>abc</div>
-//     )
-//   }
-// }
-//
-// render(document.getElementById('root'), <App />)
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-require('./css/app.styl')
+import './css/app.styl'
 
-(function(window) {
-  console.log('hello dev');
-})(window)
+class App extends Component {
+  render() {
+    return (
+      <div>abc</div>
+    )
+  }
+}
+
+render(document.getElementById('root'), <App />)
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,22 +1,10 @@
 const { resolve } = require('path')
 const validator = require('webpack-validator')
 const { getIfUtils } = require('webpack-config-utils')
-const autoprefixer = require('autoprefixer');
 const webpack = require('webpack')
 
 module.exports = env => {
   const { ifProd } = getIfUtils(env)
-  const autoprefixer = {
-    browsers: [
-      'Chrome >= 30',
-      'Firefox >= 20',
-      'IOS >= 6',
-      'Android >= 4.0',
-      'FirefoxAndroid >= 30',
-      'OperaMobile >= 12',
-      'Samsung >= 4'
-    ]
-  }
 
   const config = {
     context: resolve('src'),
